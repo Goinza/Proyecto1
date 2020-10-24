052e46bc4414702f4ab63bdfab9992f4d02572b5
Baltalesier <jere_segurado@yahoo.com>
2020-10-24 14:39:54 -0300

    PosPull1

--- mapeo.c
+++ mapeo.c
@@ -18,14 +18,34 @@ void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(v
 }
 
 tValor m_insertar(tMapeo m, tClave c, tValor v){
+    int esta=0;
 
-    Codigo=hash_code(&c);
-    *(tabla_hash+(Codigo-1))->
-    /*
-    *tEntrada = (tEntrada) malloc(sizeof(tEntrada));
+    tValor valor_reemplazado = null;
 
-    if (* == NULL) {
-        exit(MAP_ERROR_MEMORIA);
+    int Codigo=hash_code(&c);
+    tLista * lista_actual= (*m->tabla_hash+(Codigo));
+    tEntrada entrada_actual=(tEntrada) (*(l_primera(lista_actual))->elemento);
+
+    int i=0;
+    while ((i<l_longitud(lista_actual)) && (!esta)){
+        if (*m->comparador(entrada_actual->clave, c)==0){ //Si las claves son iguales, entonces copio el valor a rremplazar dentrod e la entrada
+            esta=1;
+            valor_reemplazado=entrada_actual->valor
+            entrada_actual->valor=v;
+        }
+        i++;
     }
-    */
+    if (!esta){ //Si no se encontro la entrada, se crea una nueva y se incerta en la lista.
+        tEntrada nueva_entrada= (tEntrada) malloc(sizeof(tEntrada))
+        if (*nueva_entrada == null){
+            exit(MAP_ERROR_MEMORIA);
+        }
+        nueva_entrada->clave=c;
+        nueva_entrada->valor=v;
+        l_insertar(lista_actual, l_ultima(lista_actual), nueva_entrada);
+        //--------------------
+        //ToDo: Falta revisar el factor de carga y redimensionar en caso de que se supere.
+        //-------------------
+    }
+    return valor_reemplazado;
 }
