840c81c1bb21d2afcd831e531a6d9a5b57ecd023
Goinza <gonzalo-am@hotmail.com>
2020-10-26 17:20:47 -0300

    Actualizo mapeo.c

--- mapeo.c
+++ mapeo.c
@@ -3,6 +3,10 @@
 #include "mapeo.h"
 #include "lista.h"
 
+void eliminarEntrada(void * en) {
+    free(en);
+}
+
 void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(void *, void*)) {
     *m = (tMapeo) malloc(sizeof(tMapeo));
 
@@ -22,6 +26,47 @@ void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(v
     }
 }
 
+void m_eliminar(tMapeo m, tClave c, void (*fEliminarC)(void *), void (*fEliminarV)(void *)) {
+    int indice = m->hash_code(c) % m->cantidad_elementos;
+    tLista lista = *(m->tabla_hash + indice);
+
+    tPosicion pos = l_primera(lista);
+    int found = 0;
+    tEntrada en;
+    while (pos != l_fin(lista) && !found) {
+        en = (tEntrada) l_recuperar(lista, pos);
+        if (m->comparador(en->clave, c) == 0) {
+            found = 1;
+            fEliminarC(en->clave);
+            fEliminarV(en->valor);
+            l_eliminar(lista, pos, eliminarEntrada);
+        }
+        pos = l_siguiente(lista, pos);
+    }
+
+}
+
+void m_destruir(tMapeo * m, void (*fEliminarC)(void *), void (*fEliminarV)(void *)) {
+    tMapeo map = *m;
+    int count = map->longitud_tabla;
+    int i;
+    tLista lista;
+    tPosicion pos;
+    tEntrada en;
+    for (i=0; i<count; i++) {
+        lista = *(map->tabla_hash + i);
+        pos = l_primera(lista);
+        while (pos != l_fin(lista)) {
+            en = (tEntrada) l_recuperar(lista, pos);
+            fEliminarC(en->clave);
+            fEliminarV(en->valor);
+            pos = l_siguiente(lista, pos);
+        }
+        l_destruir(map->tabla_hash + i, eliminarEntrada);
+    }
+    free(m);
+}
+
 tValor m_recuperar(tMapeo m, tClave c) {
     int indice = m->hash_code(c) % m->cantidad_elementos;
     tLista lista = *(m->tabla_hash + indice);
@@ -30,13 +75,14 @@ tValor m_recuperar(tMapeo m, tClave c) {
     int found = 0;
     tEntrada en;
     tValor val = NULL;
-    while (pos != NULL && !found) {
+    while (pos != l_fin(lista) && !found) {
         en = (tEntrada) l_recuperar(lista, pos);
         if (m->comparador(en->clave, c) == 0) {
             //clave de en == clave c, se encontro la entrada correcta
             val = en->valor;
             found = 1;
         }
+        pos = l_siguiente(lista, pos);
     }
 
     return val;
